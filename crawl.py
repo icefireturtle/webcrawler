f07c1f23d613ecceca3f719006ec7e7db34f2762
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-02-04 22:02:33 -0600

    second commit created more unit tests and added larger logic layer for stub function including general normalization checks for string starts and ends and beginning url parse

--- crawl.py
+++ crawl.py
@@ -3,13 +3,26 @@ from urllib.parse import urlparse
 def normalize_url(url: str): 
     #make logic here
     parsed = urlparse(url)
-    print(f"parsed is here: {parsed}")
+    print(f"parsed is here: {parsed.netloc}")
 
     if url.startswith("http://"):
-        normalized = url[len("http://"):]
-    elif url.startswith("https://"):
-        normalized = url[len("https://"):]
-    else:
-        normalized = url
 
-    return normalized
+        if url[len(url)-1] == "/":
+            if "www." in parsed.netloc:
+                
+                normal = url.replace("www.", "")
+                print(f"heyooo {normal}")
+                return normal[len("http://"):len(normal)-1]
+            
+            return url[len("http://"):len(url)-1]
+        
+        return url[len("http://"):]
+
+    elif url.startswith("https://"):
+        if url[len(url)-1] == "/":
+            return url[len("https://"):len(url)-1]
+        
+        return url[len("https://"):]
+
+    else:
+        return url
--- test_crawl.py
+++ test_crawl.py
@@ -29,5 +29,12 @@ class TestCrawlHTTPSlash(unittest.TestCase):
         expected = "blog.boot.dev/path"
         self.assertEqual(actual, expected)
 
+class TestCrawlWWW(unittest.TestCase):
+    def test_normalize_url(self):
+        input_url = "http://www.blog.boot.dev/path/"
+        actual = normalize_url(input_url)
+        expected = "blog.boot.dev/path"
+        self.assertEqual(actual, expected)
+
 if __name__ == "__main__":
     unittest.main()
