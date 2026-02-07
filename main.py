7e8b8dd61c7be2b1339655ad63f481335ae36eff
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-02-06 21:26:02 -0600

    working on main adding cli function with checks using sys

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from webcrawler!")
+import sys
 
+def main():
+    #print("Hello from webcrawler!")
+
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    else:
+        print(f"starting crawl of: {sys.argv[1]}")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
